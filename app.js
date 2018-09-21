45115627906cf683625cc7820d957221bed7c965
ElementG9 <garentyler@gmail.com>
2018-09-21 09:52:36 -0600

    Created files

---
+++ app.js
@@ -0,0 +1,16 @@
+// config
+var startURL = "";
+var wanted = "";
+
+// modules
+const request = require("request");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+// sub functions
+var loadPage = (url) => {};
+var getLinks = (page) => {};
+var catLinks = (links) => {};
+
+// main function
+var crawl = (url) => {};
